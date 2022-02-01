0c147fcfbed8e9bf633cee35ead236384545ce4a
MukundRoy123456 <mukundroy1492@gmail.com>
2022-02-01 12:47:11 +0530

    all files are added

---
+++ src/sessions/session1.java
@@ -0,0 +1,17 @@
+package sessions;
+
+import java.util.Scanner;
+
+public class session1 {
+
+	public static void main(String[] args) {
+		String su="0752001010983 :Grandparent created printer :PTR7438-printer ";
+		String str=su.substring(0, 13);
+		System.out.println(str);
+		//System.out.println(su.replaceAll("[^0-9]", ""));
+
+		}
+		
+	}
+
+
